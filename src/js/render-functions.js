5ebf68c1b9bd40627d895f0e0200e242e95712f8
Vladislav <vvllaadd73@gmail.com>
2025-10-28 06:12:34 +0200

    End

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,41 +5,48 @@ const loadMoreButton = document.querySelector('.load_more');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
+let lightbox;
+
 export function createGallery(images) {
 	const galleryHTML = images
-		.map(image => `
-			<li class="cart">
-			<a class="photo_link" href="${image.largeImageURL}">
-				<img class="photo_image" src="${image.webformatURL}" alt="${image.tags}" />
-			</a>
-			<div class="photo_info">
-				<p><b>Likes:</b> ${image.likes}</p>
-				<p><b>Views:</b> ${image.views}</p>
-				<p><b>Comments:</b> ${image.comments}</p>
-				<p><b>Downloads:</b> ${image.downloads}</p>
-			</div>
-		</li>`)
+		.map(
+			image => `
+      <li class="cart">
+        <a class="photo_link" href="${image.largeImageURL}">
+          <img class="photo_image" src="${image.webformatURL}" alt="${image.tags}" />
+        </a>
+        <div class="photo_info">
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+      </li>`
+		)
 		.join('');
 
 	gallery.insertAdjacentHTML('beforeend', galleryHTML);
-	lightbox.refresh();
+
+	// 🟢 Якщо lightbox вже існує — оновлюємо
+	if (lightbox) {
+		lightbox.refresh();
+	} else {
+		// 🆕 Ініціалізуємо вперше
+		lightbox = new SimpleLightbox('.gallery a', {
+			captionsData: 'alt',
+			captionDelay: 250,
+		});
+	}
+
+	// 🟢 Додаємо обробники кожного разу (бо refresh може їх "зламати")
+	lightbox.on('show.simplelightbox', () => {
+		document.body.style.overflow = 'hidden';
+	});
+
+	lightbox.on('close.simplelightbox', () => {
+		document.body.style.overflow = '';
+	});
 }
-
-const lightbox = new SimpleLightbox('.gallery a', {
-	captionsData: 'alt',
-	captionDelay: 250,
-});
-
-
-lightbox.on('show.simplelightbox', () => {
-	document.body.style.overflow = 'hidden';
-	console.log('123456');
-
-});
-
-lightbox.on('close.simplelightbox', () => {
-	document.body.style.overflow = '';
-});
 
 export function clearGallery() {
 	gallery.innerHTML = '';
